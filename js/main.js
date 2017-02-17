1ddadff2c5d8519b1789a403ef98530ad37e79f5
aheitzen <alexaheitzenrader@yahoo.com>
2017-02-17 10:22:36 -0800

    done

--- js/main.js
+++ js/main.js
@@ -19,10 +19,10 @@ var elTiles = document.getElementById('tiles');
 elTiles.textContent = tiles;
 
 var elSubTotal = document.getElementById('subTotal');
-elSubTotal.textContent = subTotal;
+elSubTotal.textContent = '$ ' + subTotal;
 
 var elShipping = document.getElementById('shipping');
-elShipping.textContent = shipping;
+elShipping.textContent = '$ ' + shipping;
 
 var elGrandTotal = document.getElementById('grandTotal');
-elGrandTotal.textContent = grandTotal;
+elGrandTotal.textContent = '$ ' + grandTotal;
